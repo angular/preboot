e7fa48bc4b0f00e31ceac3af21ea1c6afbadd93b
Zak Henry <zak.henry@gmail.com>
2016-08-01 18:02:27 +0100

    build(ts declarations): Update to generate declaration files and only package those in the distribution

--- buildtasks/tsc_task.js
+++ buildtasks/tsc_task.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var size = require('gulp-size');
 var typescript = require('gulp-typescript');
 var path = require('path');
+var merge2 = require('merge2');
 
 module.exports = function (opts) {
   gulp.task('tsc', [ 'clean' ], function () {
@@ -10,9 +11,17 @@ module.exports = function (opts) {
       declaration: true
     });
 
-    return tsProject.src().
-      pipe(typescript(tsProject)).
-      pipe(size()).
-      pipe(gulp.dest(tsProject.config.compilerOptions.outDir));
+    var tsStream = tsProject.src().
+      pipe(typescript(tsProject));
+
+
+    return merge2([
+      tsStream.js.
+        pipe(size()).
+        pipe(gulp.dest(tsProject.config.compilerOptions.outDir)),
+      tsStream.dts.
+        pipe(gulp.dest(tsProject.config.compilerOptions.outDir))
+    ]);
+
   });
 };
